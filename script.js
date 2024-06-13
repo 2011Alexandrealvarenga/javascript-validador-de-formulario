29ce50f37a197734298f3ebb2ed9235a4d8a0262
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-06-13 15:30:21 -0300

    update

--- script.js
+++ script.js
@@ -1,45 +1,77 @@
-let B7Validator= {
+let B7Validator = {
     handleSubmit:(event)=>{
-        // evitar que envia o formulario, nao deixa enviar o formulario
         event.preventDefault();
         let send = true;
 
-        let inputs = document.querySelectorAll('input');
+        let inputs = form.querySelectorAll('input');
 
-        for(let i=0; i<inputs.length;i++){
+        B7Validator.clearErrors();
+
+        for(let i=0;i<inputs.length;i++) {
             let input = inputs[i];
             let check = B7Validator.checkInput(input);
-
-            // se nao retornar true quer dizer que deu erro
-            if(check !== true){
+            if(check !== true) {
                 send = false;
-                // exibir o erro
+                B7Validator.showError(input, check);
             }
-
-
         }
 
-        send = false;
-
-        if(send){
+        if(send) {
             form.submit();
         }
     },
-    checkInput:()=>{
-        // pega o que tem no atributo data-rules
-        let rules = input.getAttributes('data-rules');
+    checkInput:(input) => {
+        let rules = input.getAttribute('data-rules');
 
-        // se for diferente de nulo
-        if(rules !== null){
+        if(rules !== null) {
             rules = rules.split('|');
-            for(let k in rules){
+            for(let k in rules) {
                 let rDetails = rules[k].split('=');
+                switch(rDetails[0]) {
+                    case 'required':
+                        if(input.value == '') {
+                            return 'Campo não pode ser vazio.';
+                        }
+                    break;
+                    case 'min':
+                        if(input.value.length < rDetails[1]) {
+                            return 'Campo tem que ter pelo menos '+rDetails[1]+' caractes';
+                        }
+                    break;
+                    case 'email':
+                        if(input.value != '') {
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase())) {
+                                return 'E-mail digitado não é válido!';
+                            }
+                        }
+                    break;
+                }
             }
-
         }
-        return true;
-    }
 
+        return true;
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++) {
+            inputs[i].style = '';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0;i<errorElements.length;i++) {
+            errorElements[i].remove();
+        }
+    }
 };
 
 let form = document.querySelector('.b7validator');

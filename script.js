68a3a3b5f6ac4c0292bc34b4bb3f1bf36143faa0
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-06-13 15:11:39 -0300

    update

--- script.js
+++ script.js
@@ -2,7 +2,44 @@ let B7Validator= {
     handleSubmit:(event)=>{
         // evitar que envia o formulario, nao deixa enviar o formulario
         event.preventDefault();
+        let send = true;
+
+        let inputs = document.querySelectorAll('input');
+
+        for(let i=0; i<inputs.length;i++){
+            let input = inputs[i];
+            let check = B7Validator.checkInput(input);
+
+            // se nao retornar true quer dizer que deu erro
+            if(check !== true){
+                send = false;
+                // exibir o erro
+            }
+
+
+        }
+
+        send = false;
+
+        if(send){
+            form.submit();
+        }
+    },
+    checkInput:()=>{
+        // pega o que tem no atributo data-rules
+        let rules = input.getAttributes('data-rules');
+
+        // se for diferente de nulo
+        if(rules !== null){
+            rules = rules.split('|');
+            for(let k in rules){
+                let rDetails = rules[k].split('=');
+            }
+
+        }
+        return true;
     }
+
 };
 
 let form = document.querySelector('.b7validator');
